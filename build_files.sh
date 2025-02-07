eb2455cce54af761aaf738a3888a4e4404b715e5
MoizAnsari99 <moizansari8482@gmail.com>
2025-02-08 00:16:25 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,15 @@
-python3 -m ensurepip --default-pip
-python3 -m pip --version
+echo "BUILD START"
 
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
+# install all deps in the venv
+pip install -r requirements.txt
+
+# collect static files using the Python interpreter from venv
+python manage.py collectstatic --noinput
+
+echo "BUILD END"
